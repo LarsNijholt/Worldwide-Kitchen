adf9ad36e7718ebffc919ffdd22a2beb5eb04321
Lars Nijholt <lars.nijholt@gmail.com>
2022-06-14 12:17:01 +0200

    Removed whitespaces because i hate those

--- Assets/Scripts/Food/FoodSpawner.cs
+++ Assets/Scripts/Food/FoodSpawner.cs
@@ -4,33 +4,31 @@ using UnityEngine;
 
 namespace Assets.Food
 {
-    public class BaseFoodManager : MonoBehaviour
+    public class FoodSpawner : MonoBehaviour
     {
         [SerializeField] private List<GameObject> _asiaFoodList = new List<GameObject>();
-        [SerializeField] private List<GameObject> _asiaSpawnList = new List<GameObject>();
+        [SerializeField] private List<Transform> _asiaSpawnPoints = new List<Transform>();
 
         [SerializeField] private List<GameObject> _europeFoodList = new List<GameObject>();
-        [SerializeField] private List<GameObject> _europeSpawnList = new List<GameObject>();
+        [SerializeField] private List<Transform> _europeSpawnPoints = new List<Transform>();
 
         [SerializeField] private List<GameObject> _oceaniaFoodList = new List<GameObject>();
-        [SerializeField] private List<GameObject> _oceaniaSpawnList = new List<GameObject>();
+        [SerializeField] private List<Transform> _oceaniaSpawnPoints = new List<Transform>();
 
         [SerializeField] private List<GameObject> _africaFoodList = new List<GameObject>();
-        [SerializeField] private List<GameObject> _africaSpawnList = new List<GameObject>();
+        [SerializeField] private List<Transform> _africaSpawnPoints = new List<Transform>();
         private void Awake()
         {
-            SpawnFood(_asiaFoodList, _asiaSpawnList);
-            SpawnFood(_europeFoodList, _europeSpawnList);
-            SpawnFood(_africaFoodList, _africaSpawnList);
-            SpawnFood(_oceaniaFoodList, _oceaniaSpawnList);
+            SpawnFood(_asiaFoodList, _asiaSpawnPoints);
+            SpawnFood(_europeFoodList, _europeSpawnPoints);
+            SpawnFood(_africaFoodList, _africaSpawnPoints);
+            SpawnFood(_oceaniaFoodList, _oceaniaSpawnPoints);
         }
 
         /// <summary>
         /// Spawns the food gameobjects and takes a random index.
         /// </summary>
-        /// <param name="AreaFoodList"></param>
-        /// <param name="AreaSpawnList"></param>
-        private void SpawnFood(List<GameObject> AreaFoodList, List<GameObject> AreaSpawnList)
+        private void SpawnFood(List<GameObject> AreaFoodList, List<Transform> AreaSpawnList)
         {
             List<int> RandomList = new List<int>();
             for (int i = 0; i < AreaFoodList.Count; i++)
@@ -48,7 +46,7 @@ namespace Assets.Food
                 if (!RandomList.Contains(random))
                 {
                     RandomList.Add(random);
-                    Transform spawnlocation = AreaSpawnList[random].gameObject.GetComponent<Transform>();
+                    Transform spawnlocation = AreaSpawnList[random];
                     AreaFoodList[itr].GetComponent<Transform>().position = spawnlocation.position;
                 }
                 else
