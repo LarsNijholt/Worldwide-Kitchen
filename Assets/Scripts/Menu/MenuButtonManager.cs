c203553334639f1aea14f712e520a45af037590a
Thomas <t.van.houtum@sintlucasedu.nl>
2022-06-14 12:02:25 +0200

    Finished Africa

---
+++ Assets/Scripts/Menu/MenuButtonManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonManager : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("ThomasScene");
+    }
+    public void SettingsButton()
+    {
+        print("Penis");
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
