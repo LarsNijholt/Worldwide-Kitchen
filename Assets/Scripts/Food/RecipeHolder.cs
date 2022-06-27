df2ea12e261a7ac028773f52a9904cbd50389e1f
Lars Nijholt <61054131+LarsNijholt@users.noreply.github.com>
2022-06-27 17:40:45 +0200

    Release

--- Assets/Scripts/Food/RecipeHolder.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RecipeHolder : MonoBehaviour
-{
-    [SerializeField] public List<GameObject> IngredientList = new List<GameObject>();
-}
--- Assets/Scripts/PointSystem/Point.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Point : MonoBehaviour
-{
-    public Point NextPoint;
-    public Point PreviousPoint;
-}
